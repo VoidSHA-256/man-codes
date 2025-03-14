76cc6250b83def1d5f981722128eea25d59aed99
VoidSHA-256 <vfe333@icloud.com>
2025-03-14 12:08:15 +0200

    scraped ecas, ebs

---
+++ scrapeEbs2Knorr.js
@@ -0,0 +1,58 @@
+const { chromium } = require("playwright");
+const fs = require("fs");
+const Papa = require("papaparse");
+
+async function scrapePage(page) {
+  return await page.$$eval('td[class="style98"] table tbody tr', (rows) => {
+    const result = [];
+    for (let i = 4; i < rows.length; i++) {
+      if (rows[i].querySelectorAll("td").length !== 3) {
+        continue;
+      }
+      const row = rows[i];
+      const tds = row.querySelectorAll("td");
+      result.push({
+        code: tds[0]?.innerText,
+        description: tds[1]?.innerText,
+        steps: tds[2]?.innerText,
+      });
+    }
+
+    return result;
+  });
+}
+
+(async () => {
+  const browser = await chromium.connectOverCDP("http://localhost:9222");
+  const context = browser.contexts()[0];
+  const page = await context.newPage();
+
+  await page.goto("https://kodtruck.ru/mank2.htm");
+
+  const links = await page.$$eval('tbody[class="style39"] a', (links) =>
+    links
+      .map((link) => ({
+        href: link.href,
+      }))
+      .filter((link) => link.href.includes("man"))
+  );
+
+  const result = [];
+
+  for (link of links) {
+    await page.goto(link.href);
+    await page.waitForSelector("table");
+
+    const rows = await scrapePage(page);
+
+    result.push(...rows);
+  }
+
+  const csvData = Papa.unparse(result);
+
+  fs.writeFileSync("ebs2Knorr.csv", csvData, "utf8");
+
+  console.log("Rows:", result.length);
+
+  await browser.close();
+})();
---
+++ scrapeEbs2Wabco.js
@@ -0,0 +1,61 @@
+const { chromium } = require("playwright");
+const fs = require("fs");
+const Papa = require("papaparse");
+
+async function scrapePage(page) {
+  return await page.$$eval('td[class="style98"] table tbody tr', (rows) => {
+    const result = [];
+    for (let i = 3; i < rows.length; i++) {
+      if (rows[i].querySelectorAll("td").length !== 3) {
+        continue;
+      }
+      const row = rows[i];
+      const tds = row.querySelectorAll("td");
+      result.push({
+        code: tds[0]?.innerText,
+        description: tds[1]?.innerText,
+        steps: tds[2]?.innerText,
+      });
+    }
+
+    return result;
+  });
+}
+
+(async () => {
+  const browser = await chromium.connectOverCDP("http://localhost:9222");
+  const context = browser.contexts()[0];
+  const page = await context.newPage();
+
+  await page.goto("https://kodtruck.ru/man%20ebs%202%20wabco.htm");
+
+  const links = await page.$$eval("a", (links) =>
+    links
+      .map((link) => ({
+        text: link.innerText.trim(),
+        href: link.href,
+      }))
+      .filter(
+        (link) => link.text.startsWith("SPN") || link.href.includes("SPN")
+      )
+  );
+
+  const result = [];
+
+  for (link of links) {
+    await page.goto(link.href);
+    await page.waitForSelector("table");
+
+    const rows = await scrapePage(page);
+
+    result.push(...rows);
+  }
+
+  const csvData = Papa.unparse(result);
+
+  fs.writeFileSync("ebs2Wabco.csv", csvData, "utf8");
+
+  console.log("Rows:", result.length);
+
+  await browser.close();
+})();
