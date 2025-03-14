1f0ed4af07763ceb1131b04dbf5fc16784761fb2
VoidSHA-256 <vfe333@icloud.com>
2025-03-14 10:48:56 +0200

    scraped edc7 common rail

--- scrapeEdc17.js
+++ scrapeEdc17.js
@@ -56,7 +56,7 @@ async function scrapePage(page) {
 
   const csvData = Papa.unparse(result);
 
-  fs.writeFileSync("output.csv", csvData, "utf8");
+  fs.writeFileSync("edc17.csv", csvData, "utf8");
 
   console.log("Rows:", result.length);
 
---
+++ scrapeEdc7CommonRail.js
@@ -0,0 +1,61 @@
+const { chromium } = require("playwright");
+const fs = require("fs");
+const Papa = require("papaparse");
+
+async function scrapePage(page) {
+  return await page.$$eval('td[class="style98"] table tbody tr', (rows) => {
+    const result = [];
+    for (let i = 6; i < rows.length; i++) {
+      if (rows[i].querySelectorAll("td").length !== 3) {
+        continue;
+      }
+      const row = rows[i];
+      const tds = row.querySelectorAll("td");
+      result.push({
+        code: tds[0]?.innerText,
+        description: tds[1]?.innerText,
+        steps: tds[2]?.innerText,
+      });
+    }
+
+    return result;
+  });
+}
+
+(async () => {
+  const browser = await chromium.connectOverCDP("http://localhost:9222");
+  const context = browser.contexts()[0];
+  const page = await context.newPage();
+
+  await page.goto("https://kodtruck.ru/man%20edc%207%20CR.htm");
+
+  const edc7Links = await page.$$eval("a", (links) =>
+    links
+      .map((link) => ({
+        text: link.innerText.trim(),
+        href: link.href,
+      }))
+      .filter(
+        (link) => link.text.startsWith("EDC7") || link.href.includes("EDC7")
+      )
+  );
+
+  const result = [];
+
+  for (edc7Link of edc7Links) {
+    await page.goto(edc7Link.href);
+    await page.waitForSelector("table");
+
+    const rows = await scrapePage(page);
+
+    result.push(...rows);
+  }
+
+  const csvData = Papa.unparse(result);
+
+  fs.writeFileSync("edc7CommonRail.csv", csvData, "utf8");
+
+  console.log("Rows:", result.length);
+
+  await browser.close();
+})();
